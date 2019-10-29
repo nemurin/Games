c581bd3296a966d0a589cc63ba77916044d44476
nemurin <yh.kyud.28@gmail.com>
2019-10-30 03:37:11 +0900

    アニメーションのファイル移動とタイマー作成

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    float countTime = 0;
+    public Sprite[] num;
+    public Image[] image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime += Time.deltaTime;
+        image[0].sprite = num[(int)countTime / 10];
+        image[1].sprite = num[(int)countTime % 10];
+        image[2].sprite = num[(int)(countTime*10-(int)countTime*10)];
+        image[3].sprite = num[(int)(countTime *100 -(int)countTime*100)% 10];
+    }
+}
